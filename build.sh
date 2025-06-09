4746bf35e95f8e8e3b11b9bc9e8706070f3f0d1d
AngeluzFranco00 <ageluzfranco@prefecovgf.edu.mx>
2025-06-08 20:52:22 -0600

    [UPDATE] Modicacion en build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -o errexit
 
-# Para sistemas basados en Debian (Render usa Ubuntu)
-sudo apt-get update
-sudo apt-get install -y python3-dev default-libmysqlclient-dev build-essential
+# Instalar dependencias del sistema (sin sudo)
+apt-get update && apt-get install -y \
+    python3-dev \
+    default-libmysqlclient-dev \
+    build-essential
 
+# Instalar dependencias de Python
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Configuración Django
 python manage.py collectstatic --noinput
 python manage.py migrate
