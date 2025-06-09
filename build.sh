cae7fd3aee718ac5e24b5ed44c1eae47e6eaba31
AngeluzFranco00 <ageluzfranco@prefecovgf.edu.mx>
2025-06-08 21:23:36 -0600

    [UPDATE] Modicacion de build.sh y start.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -o errexit
 
-# Instalar dependencias del sistema (sin sudo)
-apt-get update && apt-get install -y \
-    python3-dev \
-    default-libmysqlclient-dev \
-    build-essential
-
-# Instalar dependencias de Python
-pip install --upgrade pip
 pip install -r requirements.txt
-
-# Configuración Django
 python manage.py collectstatic --noinput
 python manage.py migrate
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 set -o errexit
+
 gunicorn Inscripciones.wsgi:application --bind 0.0.0.0:$PORT
